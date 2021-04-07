4de66fa14f1ef2cd6fb02c12d485e8643e98a691
BerejMan <tbm6040@gmail.com>
2021-04-07 21:10:41 +0300

    recreating Server and Client part

--- UserChat/src/code/Client.java
+++ UserChat/src/code/Client.java
@@ -1,48 +1,143 @@
 package code;
+import java.net.*;
 import java.io.*;
-import java.net.Socket;
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class Client {
+//THANKS VERY MUCH izotopraspadov from JavaRush ^_^
 
-    private static Socket clientSocket; 
-    private static BufferedReader reader; 
+class ClientSomthing {
     
-    private static BufferedReader in; 
-    private static BufferedWriter out; 
-
-    public static void main(String[] args) {
-    	Scanner log = new Scanner(System.in);
-    	
+    private Socket socket;
+    private BufferedReader in; // поток чтения из сокета
+    private BufferedWriter out; // поток чтения в сокет
+    private BufferedReader inputUser; // поток чтения с консоли
+    private String addr; // ip адрес клиента
+    private int port; // порт соединения
+    private String nickname; // имя клиента
+    private Date time;
+    private String dtime;
+    private SimpleDateFormat dt1;
+    
+    /**
+     * для создания необходимо принять адрес и номер порта
+     *
+     * @param addr
+     * @param port
+     */
+    
+    public ClientSomthing(String addr, int port) {
+        this.addr = addr;
+        this.port = port;
         try {
-            try {
-            	System.out.print("IP Server: ");
-               String ent = log.nextLine();
-                clientSocket = new Socket(ent, 4004); 
-                System.out.println("Connected");
-                reader = new BufferedReader(new InputStreamReader(System.in));
-               
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-               
-                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            this.socket = new Socket(addr, port);
+        } catch (IOException e) {
+            System.err.println("Socket failed");
+        }
+        try {
+            // потоки чтения из сокета / записи в сокет, и чтения с консоли
+            inputUser = new BufferedReader(new InputStreamReader(System.in));
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.pressNickname(); // get user Nickname
+            new ReadMsg().start(); // reading web in infinit 
+            new WriteMsg().start(); // writing web in infinit
+        } catch (IOException e) {
+        	
+            ClientSomthing.this.downService();
+            
+        }
 
-                System.out.println("Say");
-              
-                String word = reader.readLine();
-                
-                out.write(word + "\n");
-                out.flush();
-                String serverWord = in.readLine(); 
-                System.out.println(serverWord);
-            } finally { 
-                System.out.println("Client close");
-                clientSocket.close();
+    }
+    
+    /**
+     * getting name
+     */
+    
+    private void pressNickname() {
+        System.out.print("Press your nick: ");
+        try {
+            nickname = inputUser.readLine();
+            out.write("Hello " + nickname + "\n");
+            out.flush();
+        } catch (IOException ignored) {
+        }
+        
+    }
+    
+    /**
+     * closing socket
+     */
+    private void downService() {
+        try {
+            if (!socket.isClosed()) {
+                socket.close();
                 in.close();
                 out.close();
             }
-        } catch (IOException e) {
-            System.err.println(e);
+        } catch (IOException ignored) {}
+    }
+    
+    // reading from server
+    private class ReadMsg extends Thread {
+        public void run() {
+            
+            String str;
+            try {
+                while (true) {
+                    str = in.readLine(); 
+                    if (str.equals("stop")) {
+                        ClientSomthing.this.downService(); // харакири
+                        break; // выходим из цикла если пришло "stop"
+                    }
+                    System.out.println(str); // write mesage
+                }
+            } catch (IOException e) {
+                ClientSomthing.this.downService();
+            }
         }
+    }
+    
+    // нить отправляющая сообщения приходящие с консоли на сервер
+    public class WriteMsg extends Thread {
+        
+        @Override
+        public void run() {
+            while (true) {
+                String userWord;
+                try {
+                    time = new Date(); // текущая дата
+                    dt1 = new SimpleDateFormat("HH:mm:ss"); // берем только время до секунд
+                    dtime = dt1.format(time); // время
+                    userWord = inputUser.readLine(); // сообщения с консоли
+                    if (userWord.equals("stop")) {
+                        out.write("stop" + "\n");
+                        ClientSomthing.this.downService(); // харакири
+                        break; // выходим из цикла если пришло "stop"
+                    } else {
+                        out.write("(" + dtime + ") " + nickname + ": " + userWord + "\n"); // отправляем на сервер
+                    }
+                    out.flush(); // чистим
+                } catch (IOException e) {
+                    ClientSomthing.this.downService(); // в случае исключения тоже харакири
+                    
+                }
+                
+            }
+        }
+    }
+}
 
+public class Client {
+    
+    public static String ipAddr = "localhost";
+    public static int port = 8080;
+    
+    /**
+     * creating connection with IP and PORT
+     */
+    
+    public static void main(String[] args) {
+        new ClientSomthing(ipAddr, port);
     }
 }
--- UserChat/src/code/Server.java
+++ UserChat/src/code/Server.java
@@ -1,50 +1,174 @@
 package code;
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
+import java.net.*;
+import java.util.LinkedList;
+
+
+//  THANKS VERY MUCH izotopraspadov from JavaRush ^_^
+
+
+class ServerSomthing extends Thread {
+    
+    private Socket socket; // principal socket
+    private BufferedReader in; // reading
+    private BufferedWriter out; // writing
+    
+    /**
+     * для общения с клиентом необходим сокет (адресные данные)
+     * @param socket
+     * @throws IOException
+     */
+    
+    public ServerSomthing(Socket socket) throws IOException {
+        this.socket = socket;
+        // если потоку ввода/вывода приведут к генерированию искдючения, оно проброситься дальше
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        Server.story.printStory(out); // поток вывода передаётся для передачи истории последних 10
+        // сооюбщений новому поключению
+        start(); // вызываем run()
+    }
+    @Override
+    public void run() {
+        String word;
+        try {
+            // первое сообщение отправленное сюда - это никнейм
+            word = in.readLine();
+            try {
+                out.write(word + "\n");
+                out.flush(); // flush() нужен для выталкивания оставшихся данных
+                // если такие есть, и очистки потока для дьнейших нужд
+            } catch (IOException ignored) {}
+            try {
+                while (true) {
+                    word = in.readLine();
+                    if(word.equals("stop")) {
+                        this.downService(); // харакири
+                        break; // если пришла пустая строка - выходим из цикла прослушки
+                    }
+                    System.out.println("Echoing: " + word);
+                    Server.story.addStoryEl(word);
+                    for (ServerSomthing vr : Server.serverList) {
+                        vr.send(word); // отослать принятое сообщение с привязанного клиента всем остальным влючая его
+                        //TBM - Sending only for ONE person TODO
+                    }
+                }
+            } catch (NullPointerException ignored) {}
+
+            
+        } catch (IOException e) {
+            this.downService();
+        }
+    }
+    
+    /**
+     * отсылка одного сообщения клиенту по указанному потоку
+     * @param msg
+     */
+    private void send(String msg) {
+        try {
+            out.write(msg + "\n");
+            out.flush();
+        } catch (IOException ignored) {}
+        
+    }
+    
+    /**
+     * закрытие сервера
+     * прерывание себя как нити и удаление из списка нитей
+     */
+    private void downService() {
+            try {
+            if(!socket.isClosed()) {
+                socket.close();
+                in.close();
+                out.close();
+                for (ServerSomthing vr : Server.serverList) {
+                    if(vr.equals(this)) vr.interrupt();
+                    Server.serverList.remove(this);
+                }
+            }
+        } catch (IOException ignored) {}
+    }
+}
+
+/**
+ * класс хранящий в ссылочном приватном
+ * списке информацию о последних 10 (или меньше) сообщениях
+ */
+
+class Story {
+    
+    private LinkedList<String> story = new LinkedList<>();
+    
+    /**
+     * добавить новый элемент в список
+     * @param el
+     */
+    
+    public void addStoryEl(String el) {
+        // если сообщений больше 10, удаляем первое и добавляем новое
+        // иначе просто добавить
+        if (story.size() >= 10) {
+            story.removeFirst();
+            story.add(el);
+        } else {
+            story.add(el);
+        }
+    }
+    
+    /**
+     * отсылаем последовательно каждое сообщение из списка
+     * в поток вывода данному клиенту (новому подключению)
+     * @param writer
+     */
+    
+    public void printStory(BufferedWriter writer) {
+        if(story.size() > 0) {
+            try {
+                writer.write("History messages" + "\n");
+                for (String vr : story) {
+                    writer.write(vr + "\n");
+                }
+                writer.write("/...." + "\n");
+                writer.flush();
+            } catch (IOException ignored) {}
+            
+        }
+        
+    }
+}
 
 public class Server {
 
-    private static Socket clientSocket; 
-    private static ServerSocket server; 
-    private static BufferedReader in; 
-    private static BufferedWriter out; 
-
-    public static void main(String[] args) {
-    	Scanner log = new Scanner(System.in);
+    public static final int PORT = 8080; //TODO work in internet
+    public static LinkedList<ServerSomthing> serverList = new LinkedList<>(); // array of usr connections
+    // сервера, слушающих каждый своего клиента
+    public static Story story; // история переписки
+    
+    /**
+     * @param args
+     * @throws IOException
+     */
+    
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(PORT);
+        story = new Story();
+        System.out.println("Server Started");
         try {
-            try  {
-                server = new ServerSocket(4004); 
-                System.out.println("Server Started"); 
-                clientSocket = server.accept(); 
-               
-                try { 
-                  
-                    
-                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                  
-                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-                    
-                    
-                    String word = in.readLine();
-                    System.out.println(word);
-                    
-                    out.write("Sys Admin says: " + word + "\n");
-                    out.flush(); 
-
-                } finally { 
-                    clientSocket.close();
-                    
-                    in.close();
-                    out.close();
+            while (true) {
+                // Блокируется до возникновения нового соединения:
+                Socket socket = server.accept();
+                try {
+                    serverList.add(new ServerSomthing(socket)); // добавить новое соединенние в список
+                } catch (IOException e) {
+                    // Если завершится неудачей, закрывается сокет,
+                    // в противном случае, нить закроет его:
+                    socket.close();
                 }
-            } finally {
-                System.out.println("Server Closed");
-                    server.close();
             }
-        } catch (IOException e) {
-            System.err.println(e);
+        } finally {
+            server.close();
         }
     }
 }
